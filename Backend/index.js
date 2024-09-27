810caadbfa1a02da93d850aee3bb9946dbc8b53b
Sambuddha Biswas <38996553+XiLucas@users.noreply.github.com>
2024-09-27 23:59:31 +0530

    Slight changes in async mongo

--- Backend/index.js
+++ Backend/index.js
@@ -8,16 +8,14 @@ require("dotenv").config();
 app.use(express.json()); //HTTP Deep Dive
 const { default: mongoose } = require("mongoose");
 
-{
-  async () => {
-    try {
-      await mongoose.connect(process.env.db_todo);
-      console.log("Connected to mongoDB");
-    } catch (error) {
-      console.log("Error occured");
-    }
-  };
-}
+async () => {
+  try {
+    await mongoose.connect(process.env.db_todo);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    console.log("Error occured");
+  }
+};
 
 const JWT_SECRET = process.env.jwt_secret;
 
